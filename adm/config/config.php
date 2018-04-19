1a3efa0818600164d7801570aad289b9f8724eff
林泽全 <LINZEQUAN958@pingan.com.cn>
2018-04-19 14:20:12 +0800

    删除

--- adm/config/config.php
+++ adm/config/config.php
@@ -373,5 +373,5 @@ $config['pms_opts']= array(
 			'special'	=>8
 );
 $config['pms_len'] = count($config['pms_opts']);
-$config['root_dir'] = '/Users/linzequan958/work/priv/github/linzequan/sdvenus';
+$config['root_dir'] = '/Users/eric/work/priv/github/linzequan/sdvenus';
 $config['photo_dir'] = '/www/res/photo/';
